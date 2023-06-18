01efbefb38f8b9642a9800f9c15af627cf71cf39
Gennadiy Popov <popov.gennadiy.87@gmail.com>
2023-06-18 17:10:58 +0300

    remove gitignore files

--- index.js
+++ index.js
@@ -1,13 +1,31 @@
 "use strict";
 const createRouter = require("@arangodb/foxx/router");
+const db = require("@arangodb").db;
+const errors = require("@arangodb").errors;
+const aql = require("@arangodb").aql;
+
 const router = createRouter();
 
 module.context.use(router);
 // continued
+
 router
-	.get("/hello-world", function (req, res) {
-		res.send("Hello World!");
+	.get("/import/accounts/:search", function (req, res) {
+		const search = req.pathParams.search;
+
+		const users = db._query(aql`
+            FOR user IN usersAlias
+                SEARCH user.displayName IN TOKENS(${search}, "text_ru")
+                SORT user.displayName
+                RETURN DISTINCT {
+                    id: user.publicId || user.id,
+                    name: user.displayName,
+                    link: CONCAT("https://yandex.ru/q/profile/", user.publicId)
+                }
+        `);
+
+		res.send(users);
 	})
-	.response(["text/plain"], "A generic greeting.")
-	.summary("Generic greeting")
-	.description("Prints a generic greeting.");
+	.response(["application/json"], "Accounts list")
+	.summary("List of accounts ready to import")
+	.description("Prints JSON with user accounts from q");
